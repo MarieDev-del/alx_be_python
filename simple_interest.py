9848ae7cb1246d195fad7db3d9acf6e230bad9f1
Mary Kafaru <marykafaru742@gmail.com>
2024-09-22 01:07:00 +0100

    Using arithmetic operator to calculate area of rectangle

---
+++ simple_interest.py
@@ -0,0 +1,11 @@
+#Using arithmetic operation to calculate simple interest
+
+#Where P is in $,T is in years and R is in % 
+P = 1000
+R = 0.05
+T = 3
+
+#formula for calculating simple interest
+interest = P * R * T
+
+print("The simple interest is:",interest)
