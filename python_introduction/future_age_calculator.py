cea4d61b3b71b616a2dde6bf01cce3c9492f0e1c
Mary Kafaru <marykafaru742@gmail.com>
2024-09-22 14:11:21 +0100

    simple future age calculator using arithmetic operator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+#prompt the user for their age
+current_age = int(input("How old are you? "))
+
+#calculate the user age for year 2050
+in_year_2023 = 2050 - 2023
+age = in_year_2023 + current_age
+
+#Result 
+print(f"in 2050,you will be {age} years old.")
