4a2be4a2198d031b1ed14923fb2f889ec8e50fa3
Mary Kafaru <marykafaru742@gmail.com>
2024-09-22 09:58:25 +0100

    Using arithmetic operator to calculate area of rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,11 @@
+#Calculate the area of rectangle using arithmetic operators
+
+#Define variables
+length = 10
+width = 5
+#Calculate area  of rectange
+area = length * width 
+#result
+print(f"The area of the rectangle is: {area}")    
+
+
