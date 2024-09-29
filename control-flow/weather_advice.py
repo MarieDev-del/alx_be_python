65d276daf7d9b51f05887414efad56448fa023dc
Mary Kafaru <marykafaru742@gmail.com>
2024-09-29 13:39:59 +0100

    weather advice  using conditional statement

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,13 @@
 weather = input("What's the weather like today?(sunny/rainy/cold): ")
+
 if weather == "sunny" or weather == "Sunny":
     print("Wear a t-shirt and sunglasses.")
+    
 elif weather == "rainy" or weather == "Rainy":
     print("Don't forget your umbrella and a raincoat.")
+    
 elif weather == "cold" or weather == "Cold":
     print("Make sure to wear coat and a scarf.")
+    
 else:
     print("Sorry, I don't have recommendations for this weather.")
